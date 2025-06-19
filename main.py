14aeafa6fd330db54127ae812bfd54cf20d0dc4c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-06-19 22:46:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-  return {"message": "Hello World"}
+async def root(name: str = None):
+  message = f"Hello {name}!" if name else "Hello World!"
+  return {"message": message}
