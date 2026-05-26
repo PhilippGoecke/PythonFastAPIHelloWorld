28adbe89f2680711d773bd99ebf0de3ead2da488
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-05-26 13:54:40 +0200

    Update podman-run-fastapi.bash

--- podman-run-fastapi.bash
+++ podman-run-fastapi.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag fastapi:demo .
 podman run --interactive --tty --publish 8000:8000 fastapi:demo
-echo "browse http://localhost:8000/"
+echo "browse http://localhost:8000/?name=Test"
